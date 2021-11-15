fa8e1b8cbd9a9cb8ed06d5d47b7da7c9a1a90ccd
panferov-2010 <panferov-2010@mail.ru>
2021-11-15 18:06:24 +0600

    Create of project, add log in

---
+++ app/javascript/components/Home/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className='home-page'>
+                    User Homepage
+                </div>
+            </React.Fragment>
+        );
+    };
+};
+
+export default Home
