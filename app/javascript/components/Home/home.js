841aa66a613aa0b065bf022de73fca2f43d251e8
panferov-2010 <panferov-2010@mail.ru>
2021-11-16 17:28:33 +0600

    Add styles

--- app/javascript/components/Home/home.js
+++ app/javascript/components/Home/home.js
@@ -5,10 +5,10 @@ export default class Home extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className='home-page'>
+                <div className='home-page-header'>
                     Домашняя страница {this.props.name}
                 </div>
-                <div>
+                <div className='home-page'>
                     Здравствуйте, {this.props.name}!
                 </div>
             </React.Fragment>
