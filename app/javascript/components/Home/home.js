d0abc580e306dfa88621eae51d95c074fcf550a9
panferov-2010 <panferov-2010@mail.ru>
2021-11-16 20:52:52 +0600

    Update Homepage

--- app/javascript/components/Home/home.js
+++ app/javascript/components/Home/home.js
@@ -5,11 +5,13 @@ export default class Home extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className='home-page-header'>
-                    Домашняя страница {this.props.name}
-                </div>
-                <div className='home-page'>
-                    Здравствуйте, {this.props.name}!
+                <div> 
+                    <div className='home-page-header'>
+                        Домашняя страница {this.props.name}
+                    </div>
+                    <div className='home-page'>
+                        Контактные данные пользователя: email - {this.props.email}, телефон - {this.props.telephone}.
+                    </div>
                 </div>
             </React.Fragment>
         );
