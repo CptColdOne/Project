4c2de62ef5cdf2a440ea49295a7233a814e892b7
panferov-2010 <panferov-2010@mail.ru>
2021-11-16 21:46:32 +0600

    Modification of Homepage

--- app/javascript/components/Home/home.js
+++ app/javascript/components/Home/home.js
@@ -2,16 +2,21 @@ import React from "react";
 import "./home.scss";
 
 export default class Home extends React.Component {
+    
+    renderHeader(){
+        return <div className='home-page-header'> Домашняя страница {this.props.name} </div>
+    }
+    
+    renderPageBody(){
+        return  <div className='home-page'> Контактные данные пользователя: email - {this.props.email}, телефон - {this.props.telephone}. </div>
+    }
+
     render() {
         return (
             <React.Fragment>
                 <div> 
-                    <div className='home-page-header'>
-                        Домашняя страница {this.props.name}
-                    </div>
-                    <div className='home-page'>
-                        Контактные данные пользователя: email - {this.props.email}, телефон - {this.props.telephone}.
-                    </div>
+                    {this.renderHeader()}
+                    {this.renderPageBody()}
                 </div>
             </React.Fragment>
         );
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :authentication_keys => [:login]
 
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   attr_writer :login
 
   def login
